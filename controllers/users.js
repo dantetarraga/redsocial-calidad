ae6bb9df4b8f01601e0a129f9893053840ba972c
dantetarraga <dante.tarragau@gmail.com>
2023-11-03 15:12:49 -0500

    jwt

--- controllers/users.js
+++ controllers/users.js
@@ -8,19 +8,65 @@ class userController {
     }
 
     static async getAllUsers(req, res) {
-        const allUsers = await Usuario.find();
-        return res.status(200).json(allUsers);
+        const token = req.headers.authorization.split(' ')[1]
+
+        if (!token)
+            return res.status(401).json({ error: 'No se ha enviado el token' })
+
+        try {
+            jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+                if (err)
+                    return res.status(403).json({ error: 'Token inválido' })
+            })
+
+            const allUsers = await Usuario.find();
+            return res.status(200).json(allUsers);
+
+        } catch (error) {
+            return res.status(500).json({ error: "Error al actualizar el usuario" });
+        }
     }
 
     static async getUser(req, res) {
-        const user = await Usuario.findById(req.params.id);
-        return res.status(200).json(user);
+        const token = req.headers.authorization.split(' ')[1]
+
+        if (!token)
+            return res.status(401).json({ error: 'No se ha enviado el token' })
+
+        try {
+            jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+                if (err)
+                    return res.status(403).json({ error: 'Token inválido' })
+            })
+            const user = await Usuario.findById(req.params.id);
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({ error: "Error al actualizar el usuario" });
+        }
+
     }
 
     static async deleteUser(req, res) {
+        const token = req.headers.authorization.split(' ')[1]
         const { id } = req.params;
-        const usuarioEliminado = await Usuario.findByIdAndDelete(id);
-        return res.status(200).json(usuarioEliminado);
+
+        if (!token)
+            return res.status(401).json({ error: 'No se ha enviado el token' })
+
+        try {
+            jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+                if (err)
+                    return res.status(403).json({ error: 'Token inválido' })
+            })
+
+            // Recupera el usuario actualizado
+            const usuarioEliminado = await Usuario.findByIdAndDelete(id);
+
+            return res.status(200).json(usuarioEliminado);
+        } catch (error) {
+            return res.status(500).json({ error: "Error al actualizar el usuario" });
+        }
+
     }
 
     static async updateUser(req, res) {
