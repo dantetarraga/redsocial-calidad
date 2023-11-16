68e96d8a576c7ee76980fd889a3bd18a8cc4f692
Stephanie Garate Cardenas <jgaratec@ulasalle.edu.pe>
2023-11-16 06:07:39 -0500

    Creando publicacion Model

---
+++ models/publicacionModel.js
@@ -0,0 +1,21 @@
+// PublicacionModel.js
+import mongoose from 'mongoose';
+
+const esquemaPublicacion = new mongoose.Schema({
+  usuario: {
+    type: String,
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+});
+
+const Publicacion = mongoose.model("Publicacion", esquemaPublicacion);
+
+export default Publicacion;
